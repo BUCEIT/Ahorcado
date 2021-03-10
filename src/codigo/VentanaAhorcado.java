16029692d84d45c5ea37a51d990d1fec42cb9664
Jaime-PC <Jaime-PC@JaimePC>
2021-03-10 13:47:07 +0100

    Ahoracado casi terminado

--- src/codigo/VentanaAhorcado.java
+++ src/codigo/VentanaAhorcado.java
@@ -5,17 +5,68 @@
  */
 package codigo;
 
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
 /**
  *
  * @author xp
  */
 public class VentanaAhorcado extends javax.swing.JFrame {
-
+    
+    String paLabraOculta = "CETYS";
+    int contadorFallos= 0;
+     
     /**
      * Creates new form VentanaAhorcado
      */
     public VentanaAhorcado() {
         initComponents();
+        //
+        dibujaImagen();
+    }
+    private void chequeaBoton(JButton boton){
+        chequeaLetra(boton.getText());
+        boton.setEnabled(false);
+        
+    }
+    private void chequeaLetra(String letra){
+        if (paLabraOculta.contains(letra)){
+            //la letra si que esta. Tengo que quitar el guion bajo
+            //y ponerla en su lugar
+        }
+        else{
+            //la letra no esta y hat que aumentar el contador de fallos
+            //y cambiar la imagen del ahoracado
+            contadorFallos++;
+            dibujaImagen();
+        }
+    }
+        private void dibujaImagen() {
+        String nombreImagen = "";
+        if (contadorFallos ==0){
+        }
+        switch(contadorFallos){
+            case 0: nombreImagen = "/imagenes/ahorcado_0.png"; break;
+            case 1: nombreImagen = "/imagenes/ahorcado_1.png"; break;
+            case 2: nombreImagen = "/imagenes/ahorcado_2.png"; break;
+            case 3: nombreImagen = "/imagenes/ahorcado_3.png"; break;
+            case 4: nombreImagen = "/imagenes/ahorcado_4.png"; break;
+            case 5: nombreImagen = "/imagenes/ahorcado_5.png"; break;
+            default : nombreImagen = "/imagenes/ahorcado_fin.png"; break;
+        }
+            //cargar la imagen correspondiente en el JLabel del imegenahorcado
+                ImageIcon miImagen = new ImageIcon(
+                new ImageIcon(getClass().getResource(nombreImagen)).getImage()
+                        .getScaledInstance(imagenAhorcado.getWidth(),
+                                    imagenAhorcado.getHeight(),
+                                    Image.SCALE_DEFAULT)
+                );
+                imagenAhorcado.setIcon(miImagen);
+        
+    
+        
     }
 
     /**
@@ -169,91 +220,91 @@ public class VentanaAhorcado extends javax.swing.JFrame {
             }
         });
 
-        letra_A15.setText("A");
+        letra_A15.setText("O");
         letra_A15.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A15ActionPerformed(evt);
             }
         });
 
-        letra_A16.setText("A");
+        letra_A16.setText("P");
         letra_A16.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A16ActionPerformed(evt);
             }
         });
 
-        letra_A17.setText("A");
+        letra_A17.setText("Q");
         letra_A17.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A17ActionPerformed(evt);
             }
         });
 
-        letra_A18.setText("A");
+        letra_A18.setText("R");
         letra_A18.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A18ActionPerformed(evt);
             }
         });
 
-        letra_A19.setText("A");
+        letra_A19.setText("S");
         letra_A19.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A19ActionPerformed(evt);
             }
         });
 
-        letra_A20.setText("A");
+        letra_A20.setText("T");
         letra_A20.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A20ActionPerformed(evt);
             }
         });
 
-        letra_A21.setText("A");
+        letra_A21.setText("U");
         letra_A21.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A21ActionPerformed(evt);
             }
         });
 
-        letra_A22.setText("A");
+        letra_A22.setText("V");
         letra_A22.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A22ActionPerformed(evt);
             }
         });
 
-        letra_A23.setText("A");
+        letra_A23.setText("W");
         letra_A23.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A23ActionPerformed(evt);
             }
         });
 
-        letra_A24.setText("A");
+        letra_A24.setText("X");
         letra_A24.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A24ActionPerformed(evt);
             }
         });
 
-        letra_A25.setText("A");
+        letra_A25.setText("Y");
         letra_A25.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A25ActionPerformed(evt);
             }
         });
 
-        letra_A26.setText("A");
+        letra_A26.setText("Z");
         letra_A26.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A26ActionPerformed(evt);
             }
         });
 
-        letra_A27.setText("A");
+        letra_A27.setEnabled(false);
         letra_A27.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 letra_A27ActionPerformed(evt);
@@ -389,115 +440,115 @@ public class VentanaAhorcado extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void letra_AActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_AActionPerformed
-        // TODO add your handling code here:
+       chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_AActionPerformed
 
     private void letra_A1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A1ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A1ActionPerformed
 
     private void letra_A2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A2ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A2ActionPerformed
 
     private void letra_A3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A3ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A3ActionPerformed
 
     private void letra_A4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A4ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A4ActionPerformed
 
     private void letra_A5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A5ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A5ActionPerformed
 
     private void letra_A6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A6ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A6ActionPerformed
 
     private void letra_A7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A7ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A7ActionPerformed
 
     private void letra_A8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A8ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A8ActionPerformed
 
     private void letra_A9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A9ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A9ActionPerformed
 
     private void letra_A10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A10ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A10ActionPerformed
 
     private void letra_A11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A11ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A11ActionPerformed
 
     private void letra_A12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A12ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A12ActionPerformed
 
     private void letra_A13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A13ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A13ActionPerformed
 
     private void letra_A14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A14ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A14ActionPerformed
 
     private void letra_A15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A15ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A15ActionPerformed
 
     private void letra_A16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A16ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A16ActionPerformed
 
     private void letra_A17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A17ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A17ActionPerformed
 
     private void letra_A18ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A18ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A18ActionPerformed
 
     private void letra_A19ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A19ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A19ActionPerformed
 
     private void letra_A20ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A20ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A20ActionPerformed
 
     private void letra_A21ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A21ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A21ActionPerformed
 
     private void letra_A22ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A22ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A22ActionPerformed
 
     private void letra_A23ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A23ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A23ActionPerformed
 
     private void letra_A24ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A24ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A24ActionPerformed
 
     private void letra_A25ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A25ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A25ActionPerformed
 
     private void letra_A26ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A26ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A26ActionPerformed
 
     private void letra_A27ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_letra_A27ActionPerformed
-        // TODO add your handling code here:
+         chequeaBoton((JButton)evt.getSource()); //castear
     }//GEN-LAST:event_letra_A27ActionPerformed
 
     /**
