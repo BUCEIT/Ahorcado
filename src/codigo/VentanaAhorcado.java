750628a009fb81941a4b73b12f723a4834d8ebcf
Jaime-PC <Jaime-PC@JaimePC>
2021-03-14 16:49:04 +0100

    Ahoracado casi terminado

--- src/codigo/VentanaAhorcado.java
+++ src/codigo/VentanaAhorcado.java
@@ -113,7 +113,8 @@ public class VentanaAhorcado extends javax.swing.JFrame {
     
         
     }
-    }
+    
+        
 
     /**
      * This method is called from within the constructor to initialize the form.
